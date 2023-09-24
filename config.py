4acc621719c0b29d1c9a849eb8ac90f4adab28c9
Anuwa <81525953+Neo-fk@users.noreply.github.com>
2023-09-24 19:54:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('6328374317:AAE0gSh1NUL_06B10i3qWI_pcgjDRt5AtTY')
+    BOT_TOKEN = os.environ.get('6432978785:AAHFicdUfzVLdlJonJSEtmdqwijRKH65i8M')
     APP_ID = os.environ.get('13125592')
+    API_ID = os.environ.get('13125592')
     API_HASH = os.environ.get('4c08ab6b656c6e18fa3c68508ef5e5b0')
 
     #comma seperated user id of users who are allowed to use
